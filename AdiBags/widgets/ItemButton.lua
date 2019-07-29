4c9faa353ec302ca9f06df584a3ee3f75dd32683
Klix <33373965+Klixxx@users.noreply.github.com>
2019-07-29 22:37:27 +0200

    nah this didnt work

--- AdiBags/widgets/ItemButton.lua
+++ AdiBags/widgets/ItemButton.lua
@@ -404,8 +404,7 @@ function buttonProto:UpdateBorder(isolatedEvent)
 			border:SetTexture(texture)
 			border:SetVertexColor(r or 1, g or 1, b or 1, a or 1)
 		end
-		--border:SetTexCoord(x1 or 0, x2 or 1, y1 or 0, y2 or 1)
-		border:SetTexCoord(x1 or .08, x2 or .92, y1 or .08, y2 or .92) -- ElvUI Mod!
+		border:SetTexCoord(x1 or 0, x2 or 1, y1 or 0, y2 or 1)
 		border:SetBlendMode(blendMode or "BLEND")
 		border:Show()
 	end
