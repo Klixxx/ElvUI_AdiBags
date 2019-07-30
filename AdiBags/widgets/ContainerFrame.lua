06486cebc5199402176c5c1f2e1465c1b5617c15
Klix <33373965+Klixxx@users.noreply.github.com>
2019-07-30 13:05:39 +0200

    Give the bagslotbutton a backdrop! that looks dope!

--- AdiBags/widgets/ContainerFrame.lua
+++ AdiBags/widgets/ContainerFrame.lua
@@ -178,7 +178,6 @@ function containerProto:OnCreate(name, isBank, bagObject)
 	bagSlotButton:SetNormalTexture([[Interface\Buttons\Button-Backpack-Up]])
 	bagSlotButton:SetCheckedTexture([[Interface\Buttons\CheckButtonHilight]])
 	bagSlotButton:GetCheckedTexture():SetBlendMode("ADD")
-	bagSlotButton:GetCheckedTexture():SetTexCoord(unpack(ElvUI[1].TexCoords)) -- ElvUI Mod!
 	bagSlotButton:SetScript('OnClick', BagSlotButton_OnClick)
 	bagSlotButton.panel = bagSlotPanel
 	bagSlotButton:SetWidth(18)
@@ -189,7 +188,13 @@ function containerProto:OnCreate(name, isBank, bagObject)
 		L["Click to toggle the equipped bag panel, so you can change them."]
 	}, "ANCHOR_BOTTOMLEFT", -8, 0)
 	headerLeftRegion:AddWidget(bagSlotButton, 50)
+	
+	-- ElvUI Mod!
+	bagSlotButton:GetCheckedTexture():SetTexCoord(unpack(ElvUI[1].TexCoords))
 	ElvUI[1]:GetModule("Skins"):HandleIcon(bagSlotButton:GetNormalTexture()) -- ElvUI Mod!
+	if IsAddOnLoaded("ElvUI") then
+		bagSlotButton:CreateBackdrop()
+	end
 
 	local searchBox = CreateFrame("EditBox", self:GetName().."SearchBox", self, "BagSearchBoxTemplate")
 	searchBox:SetSize(130, 20)
