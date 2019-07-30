349607571dbcbfac4b2378fa2ae00e13a5cdb2fa
Klix <33373965+Klixxx@users.noreply.github.com>
2019-07-30 15:54:24 +0200

    now the buttons looks smooth, we are so close now guys!

--- AdiBags/widgets/ItemButton.lua
+++ AdiBags/widgets/ItemButton.lua
@@ -416,8 +416,8 @@ function buttonProto:UpdateBorder(isolatedEvent)
 			border:SetTexture(texture)
 			border:SetVertexColor(r or 1, g or 1, b or 1, a or 1)
 		end
-		border:SetTexCoord(x1 or 0, x2 or 1, y1 or 0, y2 or 1)
-		border:SetBlendMode(blendMode or "BLEND")
+		--border:SetTexCoord(x1 or 0, x2 or 1, y1 or 0, y2 or 1)
+		border:SetBlendMode(blendMode or "ADD")
 		border:Show()
 	end
 	if self.JunkIcon then
