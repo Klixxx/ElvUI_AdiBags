0c941bf522c3e301b54badf3dcb3d7f9283ac559
Sebastian <sebastian_lura@hotmail.com>
2020-07-16 22:54:21 +0200

    Update AdiBags

--- AdiBags/modules/Junk.lua
+++ AdiBags/modules/Junk.lua
@@ -27,6 +27,8 @@ local _G = _G
 local format = _G.format
 local GameTooltip = _G.GameTooltip
 local GetItemInfo = _G.GetItemInfo
+local hooksecurefunc = _G.hooksecurefunc
+local IsAddOnLoaded = _G.IsAddOnLoaded
 local ITEM_QUALITY_POOR = _G.LE_ITEM_QUALITY_POOR
 local ITEM_QUALITY_UNCOMMON = _G.LE_ITEM_QUALITY_UNCOMMON
 local print = _G.print
@@ -77,6 +79,20 @@ function mod:OnEnable()
 	wipe(cache)
 	addon.RegisterSectionHeaderScript(self, 'OnTooltipUpdate', 'OnTooltipUpdateSectionHeader')
 	addon.RegisterSectionHeaderScript(self, 'OnClick', 'OnClickSectionHeader')
+	if not self.hooked then
+		if IsAddOnLoaded('Scrap_Merchant') then
+			self:ADDON_LOADED('OnEnable', 'Scrap_Merchant')
+		else
+			self:RegisterEvent('ADDON_LOADED')
+		end
+	end
+end
+
+function mod:ADDON_LOADED(_, name)
+	if name ~= 'Scrap_Merchant' then return end
+	self:UnregisterEvent('ADDON_LOADED')
+	self:HookScrap()
+	self.hooked = true
 end
 
 function mod:OnDisable()
@@ -243,7 +259,7 @@ if Scrap and type(Scrap.IsJunk) == "function" then
 		return (force or prefs.sources.Scrap) and Scrap:IsJunk(itemId)
 	end
 
-	local function updateScrap()
+	local function hook()
 		if prefs.sources.Scrap then
 			wipe(cache)
 			addon:SendMessage("AdiBags_FiltersChanged")
@@ -251,15 +267,18 @@ if Scrap and type(Scrap.IsJunk) == "function" then
 	end
 
 	if Scrap.HookScript then
-		Scrap:HookScript('OnReceiveDrag', updateScrap)
-	end
-
-	if Scrap.Merchant and Scrap.Merchant.HookScript then
-		Scrap.Merchant:HookScript('OnReceiveDrag', updateScrap)
+		-- Fallback support for older versions; no longer a ScriptObject.
+		Scrap:HookScript('OnReceiveDrag', hook)
 	end
 
 	if Scrap.ToggleJunk then
-		_G.hooksecurefunc(Scrap, "ToggleJunk", updateScrap)
+		hooksecurefunc(Scrap, 'ToggleJunk', hook)
+	end
+
+	function mod:HookScrap()
+		if Scrap.Merchant then
+			Scrap.Merchant:HookScript('OnReceiveDrag', hook)
+		end
 	end
 
 	sourceList.Scrap = "Scrap"
