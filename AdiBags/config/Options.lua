6194393700dc65ee5774ded41f80599dbad06e41
Sebastian <sebastian_lura@hotmail.com>
2020-12-19 20:02:06 +0100

    Update build 16

--- AdiBags/config/Options.lua
+++ AdiBags/config/Options.lua
@@ -274,7 +274,7 @@ local function GetOptions()
 		name = addonName..' DEV',
 		--@end-debug@]===]
 		--@non-debug@
-		name = addonName..' v1.9.20 Build 0.15',
+		name = addonName..' v1.9.20 Build 0.16',
 		--@end-non-debug@
 		type = 'group',
 		handler = addon:GetOptionHandler(addon),
